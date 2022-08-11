65df38d8fc0b5c5036e67c5eb04903549ca246e5
Paula Tkaczuk <pautk7.6@gmail.com>
2022-08-11 20:05:15 -0300

    EJERCICIO N6

--- EJERCICIO N6.sql
+++ EJERCICIO N6.sql
@@ -1,3 +1,5 @@
+/*ESTE SCRIPT ESTA HECHO EN MYSQL
+POR MARIA PAULA TKACZUK*/
 select p.nombre as nombre_del_perro,p.perro_id,h.id, h.año
 from perro as p, historial as h
 where p.perro_id = h.id and h.año =2022
