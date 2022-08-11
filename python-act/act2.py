cdcc99f6ba25ea15723b71b2ddc528c09d732313
Santiago <santiagodromerosw@gmail.com>
2022-08-11 18:56:28 -0300

    act2-santiago-romero

---
+++ python-act/act2.py
@@ -0,0 +1,9 @@
+"""
+act2_ Santiago Romero
+"""
+
+Dueño2_array = [23546987,  "Maria",  "Perez", 4789689,  "Pueyrredon  811"]
+
+for index, value in enumerate(Dueño2_array):
+    if(index not in (0,2)):
+        print(value)
